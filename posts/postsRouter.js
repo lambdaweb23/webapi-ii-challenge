ef8090872552815fce86b0ba5d6a0778318062ad
Kenna <kennaalastair@gmail.com>
2019-10-29 15:29:56 -0700

    .findById working

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -10,6 +10,20 @@ router.get('/', (req, res) => {
                console.log(err);
                res.status(500).json({error: 'The posts information could not be retrieved. '})
           });
-})
+});
+
+router.get('/:id', (req, res) => {
+     const { id } = req.params;
+     db.findById(id)
+          .then(posts => {
+               const [post] = posts;
+               console.log(post);
+               if (post.length) {
+                    res.status(200).json(post);
+               } else {
+                    res.status(404).json({error: `This post id ${id} does not exist`})
+               }
+          });
+});
 
 module.exports = router;
